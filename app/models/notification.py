7fa781941c17fae22435c3b0fc0dfb6602d7bc36
lamtranminhthu <thultm1931@ut.edu.vn>
2026-01-23 19:45:08 +0700

    Update notification.py

---
+++ app/models/notification.py
@@ -0,0 +1,6 @@
+class Notification:
+    def __init__(self, notif_id, title, content, publish_date):
+        self.notif_id = notif_id
+        self.title = title
+        self.content = content
+        self.publish_date = publish_date
