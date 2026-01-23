6ebef4a2b618da2ddff25abf4491649642fc0396
lamtranminhthu <thultm1931@ut.edu.vn>
2026-01-23 19:41:32 +0700

    Update admin.py

--- app/models/admin.py
+++ app/models/admin.py
@@ -42,3 +42,18 @@ class Admin:
         if course_id not in self.courses:
             raise ValueError("Course not found")
         del self.courses[course_id]
+
+
+# Xây dựng hệ thống Quản lý thông báo cho Admin 
+class Admin:
+    def manage_notif(self, notifications, action, notification=None):
+        if action == "create" and notification:
+            notifications.append(notification)
+            print("Notification created successfully.")
+
+        elif action == "delete" and notification:
+            notifications[:] = [
+                n for n in notifications if n.notif_id != notification.notif_id
+            ]
+            print("Notification deleted successfully.")
+
