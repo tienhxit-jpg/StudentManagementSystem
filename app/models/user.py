69be77ad50a88ebad7153cd40ec1ab1190c4c8db
Hoang Xuan Tien <tienhx.it@gmail.com>
2026-01-23 15:02:05 +0700

    Update login function

---
+++ app/models/user.py
@@ -0,0 +1,44 @@
+import hashlib
+from ..database.db_manager import DBManager
+
+class User:
+    def __init__(self, user_id: str, password: str, full_name: str, email: str, role: str):
+        self.user_id = user_id
+        self.password = password
+        self.full_name = full_name
+        self.email = email
+        self.role = role  # 'student', 'lecturer', 'admin'
+
+    @staticmethod
+    def hash_password(password: str) -> str:
+        
+        #Mã hóa mật khẩu bằng SHA-256
+
+        return hashlib.sha256(password.encode()).hexdigest()
+    
+    """
+    User passwords shall be stored in encrypted or hashed form.
+    After 5 consecutive failed login attempts, the system shall temporarily block the
+    account
+    """
+    @staticmethod
+    def login(user_id: str, password: str, db: DBManager):
+        try:
+            hashed_pass = User.hash_password(password)
+            # Tim user trong database
+            query = """
+            SELECT user_id, full_name, email, role
+            FROM users
+            WHERE user_id = ? AND password = ?
+            """
+            result = db.execute_query(query, (user_id, hashed_pass))
+            
+            if not result:
+                return None  # Đăng nhập thất bại
+            row = result[0]
+            print(f"✓ User {row['full_name']} logged in successfully as {row['role']}.")
+            return User(row['user_id'], hashed_pass, row['full_name'], row['email'], row['role'])
+        except Exception as e:
+            print(f"(!) Error during login: {e}")
+            return None
+        
