4286e50f0fe6f5326cafae8e858a2f27a1b84269
Hoang Xuan Tien <tienhx.it@gmail.com>
2026-01-25 19:59:52 +0700

    Disable init

---
+++ app/models/__init__.py
@@ -0,0 +1,15 @@
+# Models package
+# Import models directly when needed to avoid circular dependencies
+
+"""
+Available models:
+- User: Base user model
+- Student: Student model with academic functions
+- Lecturer: Lecturer model with teaching functions
+- Admin: Administrator model
+- Course: Course management
+- Schedule: Class scheduling
+- Enrollment: Course enrollment
+- Notification: Notification system
+- Grade: Grade management and GPA calculation
+"""
